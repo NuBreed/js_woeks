d347e5555224105dff608c48f5698f0e8311258e
NuBreed <ayodejiadio@gmail.com>
2024-01-06 16:58:40 +0100

    data from data.js

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,5 +1,5 @@
 // insight page js
-
+import { newsArray } from './data.js'
 const userNumber = document.querySelector('.is-number')
 let counter = 0
 setInterval(() => {
@@ -10,68 +10,6 @@ setInterval(() => {
     userNumber.textContent = `${counter}%`
   }
 }, 40)
-const newsArray = [
-  {
-    id: 1,
-    image:
-      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Hay When You Need It',
-    author: 'George Washington',
-    date_published: 'Thursday 09 2022',
-    details: {
-      details_image:
-        'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      details_title: 'Strawberry Ginger',
-
-      details_text:
-        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque molestiae accusamus maiores vero laudantium animi a dolores vel, libero, expedita blanditiis unde! Ea voluptate aspernatur in! Numquam quidem, exercitationem cupiditate saepe illum ipsum facere voluptatem iste dolorem officia fuga, reprehenderit vero vitae doloribus laudantium voluptatibus in molestias fugit hic nam cumque laborum odit explicabo! Nulla velit nemo perferendis assumenda voluptatum impedit dolorum saepe tempora quisquam aspernatur. Voluptate sequi modi quam saepe provident? Tenetur soluta iure sint maiores necessitatibus. Unde non eos omnis quaerat numquam. Sequi repellendus cumque consequatur quos eum aperiam quia quis perspiciatis amet odit natus iste minima maiores, porro officia architecto quisquam adipisci nam perferendis necessitatibus blanditiis debitis harum molestiae! Sint commodi aspernatur velit, tempora tempore consectetur, tenetur quae rem inventore quo non nam dicta earum iusto perspiciatis maxime vel cumque vero adipisci odio. Sunt assumenda, adipisci reiciendis qui ducimus molestiae ipsum doloremque quas, ut quo, perferendis accusamus laudantium. Officiis voluptatem facere consequatur provident dicta possimus illo ut, laudantium omnis, quis eligendi? Numquam obcaecati accusamus, inventore provident dolor doloremque magnam ducimus. Magnam dolor, dicta, illo ducimus impedit dignissimos ut omnis quas natus perferendis velit optio, alias quasi corrupti. Culpa mollitia ipsa quasi soluta atque veniam corporis esse nihil.',
-    },
-  },
-  {
-    id: 2,
-    image:
-      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Hay When You Need It',
-    author: 'George Washington',
-    date_published: 'Thursday 09 2022',
-    details: {
-      details_image:
-        'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      details_title: 'Strawberry Ginger',
-
-      details_text:
-        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque molestiae accusamus maiores vero laudantium animi a dolores vel, libero, expedita blanditiis unde! Ea voluptate aspernatur in! Numquam quidem, exercitationem cupiditate saepe illum ipsum facere voluptatem iste dolorem officia fuga, reprehenderit vero vitae doloribus laudantium voluptatibus in molestias fugit hic nam cumque laborum odit explicabo! Nulla velit nemo perferendis assumenda voluptatum impedit dolorum saepe tempora quisquam aspernatur. Voluptate sequi modi quam saepe provident? Tenetur soluta iure sint maiores necessitatibus. Unde non eos omnis quaerat numquam. Sequi repellendus cumque consequatur quos eum aperiam quia quis perspiciatis amet odit natus iste minima maiores, porro officia architecto quisquam adipisci nam perferendis necessitatibus blanditiis debitis harum molestiae! Sint commodi aspernatur velit, tempora tempore consectetur, tenetur quae rem inventore quo non nam dicta earum iusto perspiciatis maxime vel cumque vero adipisci odio. Sunt assumenda, adipisci reiciendis qui ducimus molestiae ipsum doloremque quas, ut quo, perferendis accusamus laudantium. Officiis voluptatem facere consequatur provident dicta possimus illo ut, laudantium omnis, quis eligendi? Numquam obcaecati accusamus, inventore provident dolor doloremque magnam ducimus. Magnam dolor, dicta, illo ducimus impedit dignissimos ut omnis quas natus perferendis velit optio, alias quasi corrupti. Culpa mollitia ipsa quasi soluta atque veniam corporis esse nihil.',
-    },
-  },
-  {
-    image:
-      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Hay When You Need It',
-    author: 'George Washington',
-    date_published: 'Thursday 09 2022',
-  },
-  {
-    image:
-      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Hay When You Need It',
-    author: 'George Washington',
-    date_published: 'Thursday 09 2022',
-  },
-  {
-    image:
-      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Hay When You Need It',
-    author: 'George Washington',
-    date_published: 'Thursday 09 2022',
-  },
-  {
-    image:
-      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Hay When You Need It',
-    author: 'George Washington',
-    date_published: 'Thursday 09 2022',
-  },
-]
 
 let articles = newsArray.map(function (element) {
   return `
@@ -109,51 +47,25 @@ const bookmarks = document.querySelectorAll('.fa-bookmark')
 bookmarks.forEach((bookmark, index) => {
   bookmark.addEventListener('click', function () {
     console.log('food', index + 1)
-    const details = articles.map((article, index_1) => {
+    const details = newsArray.map((article, index_1) => {
       console.log(index === index_1)
+      console.log(article)
       const detialsWrapper = document.querySelectorAll(
         '.main-content-body-right'
       )
       if (index === index_1) {
-        detialsWrapper.innerHTML = `
-       
-            <div>
+        bookmark.style.color = 'red'
+        detialsWrapper.innerHTML = `<div>
               <img
                 src="${article.details.details_image}"
                 alt=""
                 class="card-details-img"
               />
-              <h2 class="card-details-title">Strawberry Ginger</h2>
+              <h2 class="card-details-title">${article.details.details_title}</h2>
               <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque
-                molestiae accusamus maiores vero laudantium animi a dolores vel,
-                libero, expedita blanditiis unde! Ea voluptate aspernatur in!
-                Numquam quidem, exercitationem cupiditate saepe illum ipsum
-                facere voluptatem iste dolorem officia fuga, reprehenderit vero
-                vitae doloribus laudantium voluptatibus in molestias fugit hic
-                nam cumque laborum odit explicabo! Nulla velit nemo perferendis
-                assumenda voluptatum impedit dolorum saepe tempora quisquam
-                aspernatur. Voluptate sequi modi quam saepe provident? Tenetur
-                soluta iure sint maiores necessitatibus. Unde non eos omnis
-                quaerat numquam. Sequi repellendus cumque consequatur quos eum
-                aperiam quia quis perspiciatis amet odit natus iste minima
-                maiores, porro officia architecto quisquam adipisci nam
-                perferendis necessitatibus blanditiis debitis harum molestiae!
-                Sint commodi aspernatur
+                ${article.details.details_title}
               </p>
-              <p>
-                , velit, tempora tempore consectetur, tenetur quae rem inventore
-                quo non nam dicta earum iusto perspiciatis maxime vel cumque
-                vero adipisci odio. Sunt assumenda, adipisci reiciendis qui
-                ducimus molestiae ipsum doloremque quas, ut quo, perferendis
-                accusamus laudantium. Officiis voluptatem facere consequatur
-                provident dicta possimus illo ut, laudantium omnis, quis
-                eligendi? Numquam obcaecati accusamus, inventore provident dolor
-                doloremque magnam ducimus. Magnam dolor, dicta, illo ducimus
-                impedit dignissimos ut omnis quas natus perferendis velit optio,
-                alias quasi corrupti. Culpa mollitia ipsa quasi soluta atque
-                veniam corporis esse nihil.
-              </p>
+             
             </div>
          
         
---
+++ Weather App/data.js
@@ -0,0 +1,62 @@
+export const newsArray = [
+  {
+    id: 1,
+    image:
+      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Hay When You Need It',
+    author: 'George Washington',
+    date_published: 'Thursday 09 2022',
+    details: {
+      details_image:
+        'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      details_title: 'Strawberry Ginger',
+
+      details_text:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque molestiae accusamus maiores vero laudantium animi a dolores vel, libero, expedita blanditiis unde! Ea voluptate aspernatur in! Numquam quidem, exercitationem cupiditate saepe illum ipsum facere voluptatem iste dolorem officia fuga, reprehenderit vero vitae doloribus laudantium voluptatibus in molestias fugit hic nam cumque laborum odit explicabo! Nulla velit nemo perferendis assumenda voluptatum impedit dolorum saepe tempora quisquam aspernatur. Voluptate sequi modi quam saepe provident? Tenetur soluta iure sint maiores necessitatibus. Unde non eos omnis quaerat numquam. Sequi repellendus cumque consequatur quos eum aperiam quia quis perspiciatis amet odit natus iste minima maiores, porro officia architecto quisquam adipisci nam perferendis necessitatibus blanditiis debitis harum molestiae! Sint commodi aspernatur velit, tempora tempore consectetur, tenetur quae rem inventore quo non nam dicta earum iusto perspiciatis maxime vel cumque vero adipisci odio. Sunt assumenda, adipisci reiciendis qui ducimus molestiae ipsum doloremque quas, ut quo, perferendis accusamus laudantium. Officiis voluptatem facere consequatur provident dicta possimus illo ut, laudantium omnis, quis eligendi? Numquam obcaecati accusamus, inventore provident dolor doloremque magnam ducimus. Magnam dolor, dicta, illo ducimus impedit dignissimos ut omnis quas natus perferendis velit optio, alias quasi corrupti. Culpa mollitia ipsa quasi soluta atque veniam corporis esse nihil.',
+    },
+  },
+  {
+    id: 2,
+    image:
+      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Hay When You Need It',
+    author: 'George Washington',
+    date_published: 'Thursday 09 2022',
+    details: {
+      details_image:
+        'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      details_title: 'Strawberry Ginger',
+
+      details_text:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque molestiae accusamus maiores vero laudantium animi a dolores vel, libero, expedita blanditiis unde! Ea voluptate aspernatur in! Numquam quidem, exercitationem cupiditate saepe illum ipsum facere voluptatem iste dolorem officia fuga, reprehenderit vero vitae doloribus laudantium voluptatibus in molestias fugit hic nam cumque laborum odit explicabo! Nulla velit nemo perferendis assumenda voluptatum impedit dolorum saepe tempora quisquam aspernatur. Voluptate sequi modi quam saepe provident? Tenetur soluta iure sint maiores necessitatibus. Unde non eos omnis quaerat numquam. Sequi repellendus cumque consequatur quos eum aperiam quia quis perspiciatis amet odit natus iste minima maiores, porro officia architecto quisquam adipisci nam perferendis necessitatibus blanditiis debitis harum molestiae! Sint commodi aspernatur velit, tempora tempore consectetur, tenetur quae rem inventore quo non nam dicta earum iusto perspiciatis maxime vel cumque vero adipisci odio. Sunt assumenda, adipisci reiciendis qui ducimus molestiae ipsum doloremque quas, ut quo, perferendis accusamus laudantium. Officiis voluptatem facere consequatur provident dicta possimus illo ut, laudantium omnis, quis eligendi? Numquam obcaecati accusamus, inventore provident dolor doloremque magnam ducimus. Magnam dolor, dicta, illo ducimus impedit dignissimos ut omnis quas natus perferendis velit optio, alias quasi corrupti. Culpa mollitia ipsa quasi soluta atque veniam corporis esse nihil.',
+    },
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Hay When You Need It',
+    author: 'George Washington',
+    date_published: 'Thursday 09 2022',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Hay When You Need It',
+    author: 'George Washington',
+    date_published: 'Thursday 09 2022',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Hay When You Need It',
+    author: 'George Washington',
+    date_published: 'Thursday 09 2022',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/219794/pexels-photo-219794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Hay When You Need It',
+    author: 'George Washington',
+    date_published: 'Thursday 09 2022',
+  },
+]
